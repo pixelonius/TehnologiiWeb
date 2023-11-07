ea5c7c24b21ef2b7c303c818c71887ed396aa4a8
razvan <razvanplastoi@yahoo.com>
2023-11-07 12:27:12 +0200

    seminar7

---
+++ seminar7/node/utils/constants.js
@@ -0,0 +1,8 @@
+const orders=[
+{id:1, name:'Alex', value:100, status:'pending'},
+{id:2, name:'George', value:200, status:'pending'},
+{id:3, name:'Andrei', value:300, status:'pending'}
+
+]
+
+module.exports=orders;
