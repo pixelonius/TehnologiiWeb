9585ad7d19496a025f43151e7bc44bc57db20e78
razvan <razvanplastoi@yahoo.com>
2023-11-07 13:06:38 +0200

    Seminar 7

--- seminar7/node/index.js
+++ seminar7/node/index.js
@@ -29,6 +29,52 @@ app.get('/orders', function(req, res){
     res.status(200).json(orders);
 });
 
+app.get('/orders/:id', function(req, res){
+    const id=parseInt(req.params.id);
+    const foundOrder=orders.find(el=>el.id===id);
+
+    if (foundOrder){
+    res.status(200).json(foundOrder);
+    } else{
+        res.status(400).json({message:"Not found"});
+    }
+
+})
+
+app.post('/orders', function(req,res){
+    const newOrder = req.body;
+    newOrder.id=orders.length+1;
+    orders.push(newOrder);
+
+    res.status(200).json(newOrder);
+})
+
+app.put("/orders/:id", function(req, res){
+    const id=parseInt(req.params.id);
+
+    const foundOrderIndex=orders.findIndex(el => el.id===id)
+
+    if(foundOrderIndex!=-1){
+        orders[foundOrderIndex]={...orders[foundOrderIndex], ...req.body};
+
+        res.status(200).jsonp(orders[foundOrderIndex]);
+    }else{
+        res.status(404).json({message:'Order not found'})
+    }
+})
+
+app.delete('/orders/:id', function(req, res){
+    const id=parseInt(req.params.id);
+
+    const foundOrderIndex=orders.findIndex(el=>el.id===id);
+
+    if(foundOrderIndex!=-1){
+        orders.splice(foundOrderIndex, 1);
+
+        res.status(200).json({message:'Order deleted'});
+    } else res.status(404).json({message:"Nor found"})
+})
+
 app.listen(3000, function() {
     console.log("Server listening on 3000");
 })
